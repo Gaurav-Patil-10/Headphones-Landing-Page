cb4543c8cd9ce8907036863e3a32a7063450a421
Patil <Gaurav.Patil@Perkinelmer.com>
2023-04-25 12:55:22 +0530

    Breakpoints of mobile screen is over , under progress for tablet

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
 // show menu
 
 const navMenu = document.getElementById('nav-menu'),
-      navToggle = document.getElementById('nav-toggle'),
-      navClose = document.getElementById("nav-close");
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById("nav-close");
 
 
-    // menu show 
-    if (navToggle){
+// menu show 
+if (navToggle) {
 
-        // navtoggle clicked show-menu class added 
-        navToggle.addEventListener('click' , ()=> {
-            navMenu.classList.add('show-menu');
-        })
-    }
+    // navtoggle clicked show-menu class added 
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    })
+}
 
 
-    // menu hidden
-    if (navClose){
-        navClose.addEventListener('click' , ()=> {
-            navMenu.classList.remove("show-menu");
-        })
-    }
+// menu hidden
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove("show-menu");
+    })
+}
 
 
 // remove menu mobile
@@ -34,7 +34,7 @@ function linkAction() {
     navMenu.classList.remove('show-menu');
 }
 
-navLink.forEach(n => n.addEventListener('click' , linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction));
 
 // change background header
 
@@ -45,6 +45,18 @@ function scrollHeader() {
     if (this.scrollY >= 50) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 
-window.addEventListener('scroll' , scrollHeader);
+window.addEventListener('scroll', scrollHeader);
+
+
+// show scroll up
+
+function scrollup() {
+    const scrollUp = document.getElementById('scrollup');
+    // when the scroll is higher than 200vh add the show-scroll class to a tag
+    if (this.scrollY >= 200) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll');
+}
+
+window.addEventListener('scroll', scrollup);
+
 
 
